56a14c2add9da71afafaab21ab4d9f28cba65297
huron low <50364332+faroperfect@users.noreply.github.com>
2023-04-21 13:01:17 +0800

    Add files via upload
    it can generate a hello_world.cpp, used for test makefile env.

---
+++ hello.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ "$1" = "do" ]; then
+if [ -f hello_world.cpp ]; then
+				echo "there is already a hello_world.cpp"
+else
+
+touch hello_world.cpp
+echo "#include <iostream>" >hello_world.cpp
+echo "int main() {" >hello_world.cpp
+echo "std::cout<<"hello, world"<<endl;" >hello_world.cpp
+echo "return 0;" >hello_world.cpp
+echo "}" >hello_world.cpp
+fi
+elif [ "$1" = "undo" ]; then
+rm -i ./hello_world.cpp
+else 
+				echo "only 1 parameter is permitted, "do" make the file, "undo" deltete it."
+fi
