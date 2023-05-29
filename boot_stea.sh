24d7e48fbdd261f9c80bbfe847d4a7e8ade686f3
huron low <50364332+faroperfect@users.noreply.github.com>
2023-05-29 19:04:52 +0800

    Update boot_stea.sh

--- boot_stea.sh
+++ boot_stea.sh
@@ -6,5 +6,7 @@ rm -rf ~/Desktop/steam.out
 # echo $(ps aux | grep steam >> steam_process.md)
 kill -9 $(pgrep steam)
 echo "Staring steam..."
+
+# I hope it can output to /dev/null, but it will pirnt output to the screen if do so. Need help.
 nohup steam > ./steam.out 2>&1 &
 echo "Steam start"
