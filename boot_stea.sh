ec52324c30f2fd3f03b5251d64be37a87f43bd86
huron low <50364332+faroperfect@users.noreply.github.com>
2023-10-28 13:20:18 +0800

    Update boot_stea.sh

--- boot_stea.sh
+++ boot_stea.sh
@@ -2,11 +2,10 @@
 
 rm -rf ~/Desktop/steam.out
 
-# kill -9  `ps aux | grep "steam" | awk '{print $2}' | awk '{printf "%s ", $0} END {print ""}' ` 
+# kill -n 9  `ps aux | grep "steam" | awk '{print $2}' | awk '{printf "%s ", $0} END {print ""}' ` 
 # echo $(ps aux | grep steam >> steam_process.md)
-kill -9 $(pgrep steam)
+kill -s KILL $(pgrep steam)
 echo "Staring steam..."
-
-# I hope it can output to /dev/null, but it will pirnt output to the screen if do so. Need help.
 nohup steam > ./steam.out 2>&1 &
 echo "Steam start"
+
